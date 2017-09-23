1ee01ff0ecdd792b02e463f48486385478b74868
liuxuhan <liuxuhan2008@gmail.com>
2017-09-24 01:18:13 +0800

    complete question 2(4)

--- code/init.py
+++ code/init.py
@@ -1,34 +1,45 @@
 import numpy as np
 import csv
+import os.path
 
 
 # load bias
-def loadBias(array, file):
+def loadBias(file):
+    array = []
     with open(file) as f:
         reader = csv.reader(f)
         for row in reader:
             array.append(np.array(row[1:]).astype(float))
+    return array
 
-def loadWeight(array,file,nodes):
+
+def loadWeight(file, nodes):
+    array = []
+    stopPoint = len(nodes) -1
     with open(file) as f:
         reader = csv.reader(f)
-        layer=0
-        index=0
-        rowData = np.empty(shape=(nodes[layer], nodes[layer+1]))
+        layer = 0
+        index = 0
+        rowData = np.empty(shape=(nodes[layer], nodes[layer + 1]))
         for row in reader:
-            rowData[index] = np.array(row[1:]).astype(float)
-            index+=1
-            if(index == nodes[layer]):
+            rowData[index] = np.array(row[1:])
+            index += 1
+            if (index == nodes[layer]):
                 array.append(rowData)
-                layer+=1
-                index =0
-                if(layer==3):
-                    return
-                rowData=np.empty(shape=(nodes[layer], nodes[layer+1]))
+                layer += 1
+                index = 0
+                if (layer == stopPoint):
+                    return array
+                rowData = np.empty(shape=(nodes[layer], nodes[layer + 1]))
+
 
 # define activation function
 
 def softmax(z):
+    if z[0][0] >= 100000:
+        z = z - np.amax(z)
+    else:
+        z = np.float128(z)
     return np.exp(z) / np.sum(np.exp(z))
 
 
@@ -36,21 +47,15 @@ def relu(z):
     return np.maximum(z, 0, z)
 
 
-class layer(object):
-    def get_iter(self):
-        return []
-
-    def get_grad(self, X, output_grad):
-        return []
-
-    def get_ouput(self, X):
-        pass
-
-    def get_input_grad(self, Y, output_grad=None, T=None):
-        pass
+def D_relu(X):
+    return 1 * (X > 0)
 
 
-class relu(layer):
-    def __init__(self, n_in, n_out):
-        self.W = [];
-        self.b = [];
+def exportFile(fileName, data):
+    addr = '../e0146241/' + fileName
+    if os.path.isfile(addr):
+        os.remove(addr)
+
+    with open(addr, 'ab') as fp:
+        for item in reversed(data):
+            np.savetxt(fp, item, delimiter=',', fmt='%.16g')
--- code/main.py
+++ code/main.py
@@ -1,43 +1,36 @@
 import numpy as np
-from init import loadBias, loadWeight, relu, softmax
+from init import loadBias, loadWeight, softmax, D_relu, exportFile, relu
+
+np.set_printoptions(precision=16)
 
 
 # import matplotlib.pyplot as plt
 
-
-
-
-# print len(b_100_40_4)
-# print w_100_40_4[0].shape
-
-
+# Define softmax layer class
 class softMaxLayer():
     def __init__(self, w, b):
         self.weight = w
         self.bias = b
-
+    # cross entropy cost function on softmax function
     def cost(self, Y, T):
-        return - np.sum(np.multiply(T, np.log(Y)))
-
+        return - np.sum(np.multiply(T, np.log(Y))) / Y.shape[0]
+    # output of softmax function
     def get_output(self, X):
         return softmax(X)
+    def get_input_grad(self, Y, T):
+        return (Y - T) / Y.shape[0]
 
-    def delta(self, ):
-        return
-
-
-class Layer(object):
-    def get_output(self, X):
-        pass
-
-
-class relu(Layer):
+# Define relu layer class
+class reluLayer():
     def __init__(self, w, b):
         self.weight = w
         self.bias = b
 
     def get_output(self, X):
-        return np.maximum(X, 0, X)
+        return relu(X)
+
+    def get_input_grad(self, Y, output_grad):
+        return np.multiply(D_relu(Y), output_grad)
 
 
 def forward(input, layers):
@@ -48,30 +41,98 @@ def forward(input, layers):
         x = layer.get_output(np.dot(np.array(x), layer.weight) + layer.bias)
         outputs.append(x)
         x = outputs[-1]
-        index+=1
+        index += 1
     return outputs
 
 
+def backpropagation(activations, targets, layers):
+    output_grad = None
+    b_grads = []
+    w_grads = []
+    for layer in reversed(layers):
+        Y = activations.pop()
+        if output_grad is None:
+            input_grad = layer.get_input_grad(Y, targets)
+        else:
+            input_grad = layer.get_input_grad(Y, output_grad)
+        X = activations[-1]
+        w_grad = np.float32(X.T.dot(input_grad))
+        b_grad = np.float32(input_grad)
+        w_grads.append(w_grad)
+        b_grads.append(b_grad)
+        output_grad = input_grad.dot(layer.weight.T)
+    return w_grads, b_grads
+
+# Create a list of layer class
+def initLayer(type, w, b):
+    layers = []
+    if type == 1:
+        # 2 hidden layers
+        layers.append(reluLayer(w[0], b[0]))
+        layers.append(reluLayer(w[1], b[1]))
+        layers.append(softMaxLayer(w[2], b[2]))
+    elif type == 2:
+        # 6 * 28 hidden layer
+        for i in range(6):
+            layers.append(reluLayer(w[i],b[i]))
+        layers.append(softMaxLayer(w[6], b[6]))
+    elif type == 3:
+        # 28 * 14 hidden layer
+        for i in range(28):
+            layers.append(reluLayer(w[i], b[i]))
+        layers.append(softMaxLayer(w[28], b[28]))
+    return layers
+
+# list of dimension
+def dimInList(layers, nodes):
+    dim = [14]
+    for i in range(layers):
+        dim.append(nodes)
+    dim.append(4)
+    return dim
+
+def training(layers,x,y,wfile,bfile):
+    # forward
+    output = forward(x, layers)
+    # backpropagation
+    w_grads, b_grads = backpropagation(output, y, layers)
+    exportFile(bfile, b_grads)
+    exportFile(wfile, w_grads)
+
+
 def main():
-    y_train_raw = np.genfromtxt('../data/y_train.csv', delimiter=",").astype(np.int64)
-    y_train = np.eye(4)[y_train_raw].astype(np.int64)
-    x_train = np.genfromtxt('../data/x_train.csv', delimiter=",").astype(np.int64)
-    b_100_40_4 = []
-    w_100_40_4 = []
-    loadBias(b_100_40_4, '../data/b-100-40-4.csv')
-    loadWeight(w_100_40_4, '../data/w-100-40-4.csv', [14, 100, 40, 4])
-    layer = []
-    layer.append(relu(w_100_40_4[0], b_100_40_4[0]))
-    layer.append(relu(w_100_40_4[1], b_100_40_4[1]))
-    layer.append(softMaxLayer(w_100_40_4[2], b_100_40_4[2]))
-    output = forward(x_train[0], layer)
-    print output[3]
-    print output[3].shape
-    soft = softMaxLayer(10,1)
-    print soft.cost(output[3],[0,0,0,1])
+    # y_train_raw = np.genfromtxt('../data/y_train.csv', delimiter=",").astype(np.int64)
+    # y_train = np.eye(4)[y_train_raw].astype(np.int64)
+    # x_train = np.genfromtxt('../data/x_train.csv', delimiter=",").astype(np.int64)
+    x_test = np.array([[-1, 1, 1, 1, -1, -1, 1, -1, 1, 1, -1, -1, 1, 1]])
+    y_test = np.array([[0, 0, 0, 1]])
+
+    # load weight and bias for first NN
+    b_100_40_4 = loadBias('../data/b-100-40-4.csv')
+    w_100_40_4 = loadWeight('../data/w-100-40-4.csv', [14, 100, 40, 4])
+    layer1 = initLayer(1,w_100_40_4,b_100_40_4)
+
+    # load weight and bias for second NN
+    b_28_6_4 = loadBias('../data/b-28-6-4.csv')
+    w_28_6_4 = loadWeight('../data/w-28-6-4.csv', dimInList(6, 28))
+    layer2 = initLayer(2, w_28_6_4, b_28_6_4)
+
+    # load weight and bias for third NN
+    b_14_28_4 = loadBias('../data/b-14-28-4.csv')
+    w_14_28_4 = loadWeight('../data/w-14-28-4.csv', dimInList(28, 14))
+    layer3 = initLayer(3,w_14_28_4,b_14_28_4)
+
+    #run one-time training
+    training(layer1,x_test,y_test,'dw-100-40-4.csv','db-100-40-4.csv')
+    training(layer2, x_test, y_test, 'dw-28-6-4.csv', 'db-28-6-4.csv')
+    training(layer3, x_test, y_test, 'dw-14-28-4.csv', 'db-14-28-4.csv')
+    print 'Result generated'
+
 
 def test():
-    print np.array([[1, 2, 2, 3, 4],[1, 2, 2, 3, 4]]) + np.array([1, 2, 2, 2, 2])
+    x = np.array([[1, 2, 3]])
+    w_100_40_4 = loadWeight('../data/w-100-40-4.csv', [14, 100, 40, 4])
+    print np.around([0.37876876876876876786876868767, 1.64], decimals=16)
 
 
 if __name__ == "__main__":
