71d998c212ed826365f0f211fe7d58d1d0b0c0b4
Liu Xuhan <liu_xu@worksap.co.jp>
2017-09-26 20:00:53 +0800

    complete!

--- code/main.py
+++ code/main.py
@@ -4,7 +4,7 @@ import copy
 import matplotlib.pyplot as plt
 import shutil
 import os.path
-from init import loadBias, loadWeight, softmax, D_relu, exportFile, relu
+from utils import loadBias, loadWeight, softmax, D_relu, exportFile, relu
 
 np.set_printoptions(precision=16)
 
@@ -40,11 +40,6 @@ class reluLayer():
 
     def get_input_grad(self, Y, output_grad):
         return np.multiply(D_relu(Y), output_grad)
-
-
-def result(input, layers):
-    output = forward(input, layers)
-    return np.argmax(output[-1])
 
 
 def forward(input, layers):
@@ -101,7 +96,7 @@ def initLayer(type, w, b):
     return layers
 
 
-# list of dimension
+# list of number of node in each layers
 def dimInList(type):
     dim = [14]
     if type==1:
@@ -116,16 +111,13 @@ def dimInList(type):
     dim.append(4)
     return dim
 
-
+# one-time training
 def training(layers, x, y, wfile, bfile, isTraining, rate):
     # forward
     output = forward(x, layers)
 
-    # save copy of output
-    output_copy = copy.deepcopy(output)
-
     # backpropagation
-    w_grads, b_grads = backpropagation(output_copy, y, layers)
+    w_grads, b_grads = backpropagation(output, y, layers)
     if isTraining:
         index = 0
         for layer in reversed(layers):
@@ -135,7 +127,6 @@ def training(layers, x, y, wfile, bfile, isTraining, rate):
     else:
         exportFile(bfile, b_grads)
         exportFile(wfile, w_grads)
-
 
 def q4():
     x_test = np.array([[-1, 1, 1, 1, -1, -1, 1, -1, 1, 1, -1, -1, 1, 1]])
@@ -157,79 +148,35 @@ def q4():
     layer3 = initLayer(3, w_14_28_4, b_14_28_4)
 
     # run one-time training
-    training(layer1, x_test, y_test, 'dw-100-40-4.csv', 'db-100-40-4.csv', False, 1, [])
-    training(layer2, x_test, y_test, 'dw-28-6-4.csv', 'db-28-6-4.csv', False, 1, [])
-    training(layer3, x_test, y_test, 'dw-14-28-4.csv', 'db-14-28-4.csv', False, 1, [])
+    training(layer1, x_test, y_test, 'dw-100-40-4.csv', 'db-100-40-4.csv', False, 1)
+    training(layer2, x_test, y_test, 'dw-28-6-4.csv', 'db-28-6-4.csv', False, 1)
+    training(layer3, x_test, y_test, 'dw-14-28-4.csv', 'db-14-28-4.csv', False, 1)
     print 'Result generated'
 
-
-def loadTraningTestData():
+def loadTrainAndTestData():
+    #training data
+    x_train = np.genfromtxt('../data/x_train.csv', delimiter=",").astype(np.float32)
+    #translate class format into 0/1 format
     y_train_raw = np.genfromtxt('../data/y_train.csv', delimiter=",").astype(np.int64)
     y_train = np.eye(4)[y_train_raw].astype(np.float32)
-    x_train = np.genfromtxt('../data/x_train.csv', delimiter=",").astype(np.float32)
+
+    #test data
     x_test = np.genfromtxt('../data/x_test.csv', delimiter=",").astype(np.float32)
+    #translate class format into 0/1 format
     y_test_raw = np.genfromtxt('../data/y_test.csv', delimiter=",").astype(np.int64)
     y_test = np.eye(4)[y_test_raw].astype(np.float32)
+
     return x_train, x_test, y_train, y_test
-
-
-def generateParameter(type):
-    bias = []
-    weight = []
-    if type == 1:
-        r = math.sqrt(2.0 / 14)
-        weight.append(np.random.uniform(size=(14, 100), low=-r, high=r))
-        r = math.sqrt(2.0 / 100)
-        weight.append(np.random.uniform(size=(100, 40), low=-r, high=r))
-        r = math.sqrt(2.0 / 40)
-        weight.append(np.random.uniform(size=(40, 4), low=-r, high=r))
-        return weight, [np.zeros(shape=(1, 100)), np.zeros(shape=(1, 40)), np.zeros(shape=(1, 4))]
-    elif type == 2:
-        # bias
-        for i in range(6):
-            bias.append(np.zeros(shape=(1, 28)))
-        bias.append(np.zeros(shape=(1, 4)))
-        # weight
-        r = math.sqrt(2.0 / 14)
-        weight.append(np.random.uniform(size=(14, 28), low=-r, high=r))
-        for i in range(5):
-            r = math.sqrt(2.0 / 28)
-            weight.append(np.random.uniform(size=(28, 28), low=-r, high=r))
-        r = math.sqrt(2.0 / 28)
-        weight.append(np.random.uniform(size=(28, 4), low=-r, high=r))
-        return weight, bias
-    elif type == 3:
-        for i in range(28):
-            bias.append(np.ones(shape=(1, 14)))
-            weight.append(np.random.randn(14, 14))
-        bias.append(np.ones(shape=(1, 4)))
-        weight.append(np.random.randn(14, 4))
-        return weight, bias
-
 
 def generateParam(list):
     bias = []
     weight = []
     for i in range(len(list)-1):
+        # w is normal distribution, b is zero
         r = math.sqrt(2.0 / list[i])
         weight.append(np.random.uniform(size=(list[i], list[i+1]), low=-r, high=r))
         bias.append(np.zeros(shape=(1, list[i+1])))
     return weight,bias
-
-
-def loadinitW(path, loop):
-    w = []
-    for i in range(loop):
-        w.append(np.loadtxt(path + '/w' + str(i)))
-    return w
-
-
-def storeParameter(path, loop, w):
-    shutil.rmtree(path)
-    os.makedirs(path)
-    for i in range(loop):
-        np.savetxt(path + '/w' + str(i), w[i])
-
 
 def accuracy_cost(input, layers, target):
     output = forward(input, layers)
@@ -239,39 +186,76 @@ def accuracy_cost(input, layers, target):
     tf = np.equal(output_digit, target).all(axis=1)
     return np.float32(np.sum(tf)) / np.float32(target.shape[0]) * 100, cost
 
+def q123(type,step,rate,iter):
+    # initial weight and bias for NN and list of layers
+    if type == 1:
+        nn = '<NN-14-100-40-4>'
+        w_100_40_4, b_100_40_4 = generateParam(dimInList(1))
+        layers = initLayer(1, w_100_40_4, b_100_40_4)
+    elif type==2:
+        nn = '<NN-14-28*6-4>'
+        w_28_6_4, b_28_6_4 = generateParam(dimInList(2))
+        layers = initLayer(2, w_28_6_4, b_28_6_4)
+    elif type ==3:
+        nn = '<NN-14-14*28-4>'
+        w_14_28_4, b_14_28_4 = generateParam(dimInList(3))
+        layers = initLayer(3, w_14_28_4, b_14_28_4)
+    else:
+        print "wrong NN numer"
+        return
 
-def q123():
-    # initial weight and bias for first NN
-    w_100_40_4, b_100_40_4 = generateParam(dimInList(1))
-    layer1 = initLayer(1, w_100_40_4, b_100_40_4)
-    # initial weight and bias for second NN
-    w_28_6_4, b_28_6_4 = generateParam(dimInList(2))
-    layer2 = initLayer(2, w_28_6_4, b_28_6_4)
-    # initial weight and bias for third NN
-    w_14_28_4, b_14_28_4 = generateParam(dimInList(3))
-    layer3 = initLayer(3, w_14_28_4, b_14_28_4)
+    figName = 'NN'+str(type)+'-'+str(step)+'-'+str(rate)+'-'+str(iter)+'-train-cost.png'
+    figName2 = 'NN'+str(type)+'-'+str(step)+'-'+str(rate)+'-'+str(iter)+'-train-accuracy.png'
+    figName3 = 'NN'+str(type)+'-'+str(step)+'-'+str(rate)+'-'+str(iter)+'-test-cost.png'
+    figName4 = 'NN'+str(type)+'-'+str(step)+'-'+str(rate)+'-'+str(iter)+'-test-accuracy.png'
+    scores_train,costs_train,scores_test,costs_test = stochastic_gradient_descend(step,rate,layers,iter)
+    
+    #draw figure of cost data
+    plt.figure(0)
+    plt.plot(costs_train)
+    plt.xlabel('Iteration')
+    plt.ylabel('Cost')
+    plt.title('Cross Entropy Cost on train data set '+nn)
+    plt.grid(True)
+    if os.path.isfile(figName):
+        os.remove(figName)
+    plt.savefig(figName)
 
-    #oneScores_train, oneCosts_train, oneScores_test,oneCosts_test = stochastic_gradient_descend(50, 0.1, layer1, 50)
-    #twoScores_train, twoCosts_train, twoScores_test, twoCosts_test = stochastic_gradient_descend(50, 0.1, layer2, 100)
-    threeScores_train, threeCosts_train, threeScores_test, threeCosts_test = stochastic_gradient_descend(32, 1, layer3, 100)
+    #draw figure of accuracy data
+    plt.figure(1)
+    plt.plot(scores_train)
+    plt.xlabel('Iteration')
+    plt.ylabel('Accuracy')
+    plt.title('Accuracy in percentage on train data set '+nn)
+    plt.grid(True)
+    if os.path.isfile(figName2):
+        os.remove(figName2)
+    plt.savefig(figName2)
 
-    #line1 = plt.plot(oneCosts_train)
-    #plt.setp(line1, color='b', linewidth=1.0)
+    #draw figure of cost data
+    plt.figure(2)
+    plt.plot(costs_test)
+    plt.xlabel('Iteration')
+    plt.ylabel('Cost')
+    plt.title('Cross Entropy Cost on test data set '+nn)
+    plt.grid(True)
+    if os.path.isfile(figName3):
+        os.remove(figName3)
+    plt.savefig(figName3)
 
-    # line2 = plt.plot(twoCosts_train)
-    # plt.setp(line2, color='g', linewidth=1.0)
-    #
-    line3 = plt.plot(threeCosts_train)
-    plt.setp(line3, color='r', linewidth=1.0)
-
-    plt.ylabel('Cross Entropy Cost')
-    plt.show()
-    raw_input()
-
-
+    #draw figure of accuracy data
+    plt.figure(3)
+    plt.plot(scores_test)
+    plt.xlabel('Iteration')
+    plt.ylabel('Accuracy')
+    plt.title('Accuracy in percentage on test data set '+nn)
+    plt.grid(True)
+    if os.path.isfile(figName4):
+        os.remove(figName4)
+    plt.savefig(figName4)
 
 def stochastic_gradient_descend(step, learning_rate, layers, iter):
-    x_train, x_test, y_train, y_test = loadTraningTestData()
+    x_train, x_test, y_train, y_test = loadTrainAndTestData()
     costs_test = []
     scores_test = []
     costs_train = []
@@ -283,12 +267,11 @@ def stochastic_gradient_descend(step, learning_rate, layers, iter):
         score_train, cost_train = accuracy_cost(x_train, layers, y_train)
         score_test, cost_test = accuracy_cost(x_test, layers, y_test)
         print 'iteration % d, the accuracy on train set is %.2f , and cost is %.2f' %(j+1,score_train,cost_train)
-        print 'iteration %d, the accuracy on test set is %.2f , and cost is %.2f' % (j + 1, score_test, cost_test)
+        #print 'iteration %d, the accuracy on test set is %.2f , and cost is %.2f' % (j + 1, score_test, cost_test)
         scores_test.append(score_test)
         costs_test.append(cost_test)
         scores_train.append(score_train)
         costs_train.append(cost_train)
-    # print 'NN is trained, the accuracy on train set is %.2f , the ' % np.amax(scores)
     return scores_train, costs_train,scores_test,costs_test
 
 
@@ -303,4 +286,9 @@ def test():
 
 
 if __name__ == "__main__":
-    q123()
+    #Draw cost and accuracy plot for test and train data
+    # param: NN number (other param is set in method)
+    q123(1,32,0.015,70)
+    #q1(2,32,0.002,300)
+    # Output dw and db files
+    #q4()
